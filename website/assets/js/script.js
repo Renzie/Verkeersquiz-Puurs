81cf908693e7459b88e0c2325f84d2f1dffe0729
Renzie <omanarenzie@gmail.com>
2017-10-22 18:40:24 +0200

    update website

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -1,1 +1,20 @@
-aa
+$(document).ready(function(){
+  $('select').material_select();
+  $('.newanswer').on('click', addNewAnswer);
+});
+
+
+var addNewAnswer = function(e) {
+  e.preventDefault();
+    var newInput = '<div class="input-field col s10">' +
+      '<input placeholder="Antwoord 1" id="Antwoord-1" type="text" class="validate">' +
+      '<a class=" col s3 newanswer waves-effect waves-light btn">Voeg een nieuwe antwoord toe</a>' +
+    '</div>' +  
+    '<a class="btn-floating btn-small red">' +
+'<i class="large material-icons">mode_edit</i>' +
+'</a>'
+    ;
+
+    $(this).parent().append(newInput);
+
+}
