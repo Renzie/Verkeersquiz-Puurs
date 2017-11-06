95f70678086cfb13a6aaad59ba3c61eb862739c9
Arthur Saprunov <Arthur_Saprunov@hotmail.com>
2017-11-06 09:09:05 +0100

    added database file

--- extra/puurs.sql
+++ extra/puurs.sql
@@ -26,7 +26,10 @@ CREATE TABLE `answer` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Answer` longtext NOT NULL,
   `QuestionId` int(11) NOT NULL,
-  PRIMARY KEY (`Id`)
+  `Correct` tinyint(1) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `Question_idx` (`QuestionId`),
+  CONSTRAINT `QuestionForAwnser` FOREIGN KEY (`QuestionId`) REFERENCES `question` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -47,17 +50,14 @@ DROP TABLE IF EXISTS `answer_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `answer_user` (
-  `Id` int(11) NOT NULL,
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `UserId` int(11) NOT NULL,
-  `QuestionId` int(11) NOT NULL,
   `AwnserId` int(11) NOT NULL,
   `Time` time DEFAULT NULL,
   PRIMARY KEY (`Id`),
   KEY `Student_idx` (`UserId`),
-  KEY `Question_idx` (`QuestionId`),
   KEY `Answer_idx` (`AwnserId`),
   CONSTRAINT `Answer` FOREIGN KEY (`AwnserId`) REFERENCES `answer` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `Question` FOREIGN KEY (`QuestionId`) REFERENCES `question` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `Student` FOREIGN KEY (`UserId`) REFERENCES `user` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -105,7 +105,7 @@ DROP TABLE IF EXISTS `difficulty`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `difficulty` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `difficulty` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -128,11 +128,12 @@ DROP TABLE IF EXISTS `login`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `login` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
-  `password` longtext,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -141,7 +142,7 @@ CREATE TABLE `login` (
 
 LOCK TABLES `login` WRITE;
 /*!40000 ALTER TABLE `login` DISABLE KEYS */;
-INSERT INTO `login` VALUES (0,'art','123456');
+INSERT INTO `login` VALUES (1,'Maxime','password'),(2,'Renzie','$2y$10$HxR/xDXO3.BlUuKGTuwwOuTsPsR0fHchcx.LrTUkI105l4qsXWtAy'),(3,'Arthur','$2y$10$zHh3FtXoWLFkB2M4qWvbJ.MC13TJmVt6g4Bq.eCTXkVajfeJV3w9y');
 /*!40000 ALTER TABLE `login` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -154,10 +155,10 @@ DROP TABLE IF EXISTS `logs`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `logs` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Message` longtext,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -166,6 +167,7 @@ CREATE TABLE `logs` (
 
 LOCK TABLES `logs` WRITE;
 /*!40000 ALTER TABLE `logs` DISABLE KEYS */;
+INSERT INTO `logs` VALUES (1,'2017-10-26 12:50:33','Test'),(2,'2017-10-26 12:50:33','Test2'),(3,'2017-10-26 12:50:33','Tets3'),(4,'2017-10-26 12:50:33','Test4');
 /*!40000 ALTER TABLE `logs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -203,13 +205,13 @@ DROP TABLE IF EXISTS `question`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `question` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Question` longtext NOT NULL,
-  `Difficulty` int(11) DEFAULT NULL,
+  `Question` mediumtext NOT NULL,
+  `Difficulty` int(11) NOT NULL,
   `ImageLink` longtext,
   `Time` time DEFAULT NULL,
   PRIMARY KEY (`Id`),
-  KEY `difficulty_idx` (`Difficulty`),
-  CONSTRAINT `difficulty` FOREIGN KEY (`Difficulty`) REFERENCES `difficulty` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `Diff_idx` (`Difficulty`),
+  CONSTRAINT `Diff` FOREIGN KEY (`Difficulty`) REFERENCES `difficulty` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -220,6 +222,58 @@ CREATE TABLE `question` (
 LOCK TABLES `question` WRITE;
 /*!40000 ALTER TABLE `question` DISABLE KEYS */;
 /*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quiz`
+--
+
+DROP TABLE IF EXISTS `quiz`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quiz` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) DEFAULT NULL,
+  `Extra info` mediumtext,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quiz`
+--
+
+LOCK TABLES `quiz` WRITE;
+/*!40000 ALTER TABLE `quiz` DISABLE KEYS */;
+INSERT INTO `quiz` VALUES (1,'Howest Quiz','Dit is een test quiz'),(2,'Howest Quiz 2','Dit is een 2de test quiz');
+/*!40000 ALTER TABLE `quiz` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quiz_questions`
+--
+
+DROP TABLE IF EXISTS `quiz_questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quiz_questions` (
+  `QuizId` int(11) NOT NULL,
+  `QuestionId` int(11) NOT NULL,
+  PRIMARY KEY (`QuizId`),
+  KEY `Question_idx` (`QuestionId`),
+  KEY `_idx` (`QuestionId`),
+  CONSTRAINT `QuestionForQuiz` FOREIGN KEY (`QuestionId`) REFERENCES `question` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `Quiz` FOREIGN KEY (`QuizId`) REFERENCES `quiz` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quiz_questions`
+--
+
+LOCK TABLES `quiz_questions` WRITE;
+/*!40000 ALTER TABLE `quiz_questions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `quiz_questions` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -245,7 +299,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Arthur','Saprunov',1),(2,'Renzie','Omaña',1),(3,'Maxime','Mylle',1);
+INSERT INTO `user` VALUES (1,'Arthur','Saprunov',1),(2,'Renzie','Oma?a',1),(3,'Maxime','Mylle',1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -258,4 +312,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-25 14:54:11
+-- Dump completed on 2017-11-06  9:05:16
