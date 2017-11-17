2c9036a0c8a0d73875a39b58e587defe487b5e8f
Renzie <omanarenzie@gmail.com>
2017-11-17 15:26:28 +0100

    Question met timer waar je mee kan stoppen

--- quizsite/assets/js/script.js
+++ quizsite/assets/js/script.js
@@ -4,49 +4,54 @@
 $(function () {
     $('select').material_select();
 
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    var question = new Question('derp', 60);
+    //question.startQuestion();
+    startQuestion = setInterval(question.update, 1000);
+    $('#eindevraag').on('click', question.stopTimer);
+});
 
-    Materialize.showStaggeredList('.list')
 
-    $('.next').on('click', nextPanel);
-    startTimer(60);
-})
 
-document.addEventListener("DOMContentLoaded", function(){
+function Question(text, time) {
+    var that = this;
+
+    this.time = time;
+    this.text = text;
+    this.width = 0;
+    this.total = time;
+
+    //this.startQuestion = setInterval(that.update, 1000);
+
+
+    this.update = () => {
+        console.log("updating")
+        if (time < 0) {
+            that.stopTimer();
+        } else if (time == Math.round((this.total / 2) * 10 / 10)) {
+            Materialize.toast('Je hebt nog ' + time-- + ' Seconden!', 4000);
+        } else if (time == 10) {
+            Materialize.toast('Je hebt nog ' + time-- + ' Seconden! Schiet op!', 4000);
+        }
+        else {
+            this.width += 100 / this.total;
+            $('.determinate').css('width', this.width + '%');
+            $('.seconds').text(time-- + " sec");
+        }
+    };
+
+    this.stopTimer = () => {
+        $('.answers p input').attr('disabled', true)
+        console.log(time);
+        clearInterval(startQuestion);
+    }
+}
+
+/*document.addEventListener("DOMContentLoaded", function () {
     $('.preloader-background').delay(1000).fadeOut('slow');
 
     $('.preloader-wrapper')
         .delay(1000)
         .fadeOut();
 });
-var nextPanel = (e) =>{
-    e.preventDefault();
-    $('.carousel').carousel('next');
-}
 
-var startTimer = function(time){ //todo wat als er geen tijd voorzien wordt?
-    var width = 0;
-    var total = time;
-    var interval = setInterval(update,1000);
-
-
-    function update() {
-
-        if (time < 0) {
-            endTimer();
-        } else {
-            width += 100 / total;
-            $('.determinate').css('width', width + '%');
-            //$('.timer').css('width', (time/total)*100 + 'vw')
-            $('.seconds').text(time-- + " sec");
-
-            console.log(width);
-        }
-    };
-
-    function endTimer(){
-        clearInterval(interval);
-        $('.answers p input').attr('disabled', true)
-    }
-};
-
+*/
