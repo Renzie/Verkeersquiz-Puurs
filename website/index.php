2f2dcadf6a64d06ab77cc60cbfcae13340c8e6d3
Sky_Dragonsz <Arthur_Saprunov@hotmail.com>
2017-11-18 18:59:29 +0100

    small bugfix

--- website/index.php
+++ website/index.php
@@ -9,7 +9,6 @@ require_once "head.html";
 require_once "includes/Database.class.php";
 require_once "includes/UserTools.class.php";
 require_once "includes/View.class.php";
-require_once "index.html";
 
 
 ?>
