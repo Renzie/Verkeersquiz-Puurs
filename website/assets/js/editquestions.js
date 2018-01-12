611ed8dd8b60347725273c86f78cc829f041a326
Sky_Dragonsz <Arthur_Saprunov@hotmail.com>
2018-01-12 14:55:21 +0100

    Moved function 'addHTMLToNav'

--- website/assets/js/editquestions.js
+++ website/assets/js/editquestions.js
@@ -13,7 +13,6 @@ function bindEvents(){
     $('.question').on('click','.removeQuestion',removeQuestion);
     $(".dropdown-button").dropdown();
     $(".button-collapse").sideNav();
-    addHTMLToNav();
     $('form').on('change',updateQuestion);
 }
 
@@ -224,18 +223,4 @@ var markCorrect = function(){
        $(this).attr("correct", 0)
      }
    });
-}
-
-var addHTMLToNav= function(){
-
-      var url_string = window.location.href;
-      var url = new URL(url_string);
-      var quizId = url.searchParams.get("id");
-      var html = `<li><div class="divider"></div></li>
-                  <li><a href="aantal.php?id=${quizId}"><i class="material-icons">format_list_numbered</i>Quiz Template</a></li>
-                  <li><a href="moeilijkheidsgraad.php?id=${quizId}"><i class="material-icons">format_align_left</i>Moeilijkheidsgraad</a></li>
-                  <li><a href="categories.php?id=${quizId}"><i class="material-icons">sort</i>Categorieën</a></li>
-                  <li><a href="dashboard.php?id=${quizId}"><i class="material-icons">equalizer</i>Statistics</a></li>`;
-
-      $("#slide-out").append(html);
 }
--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -3,4 +3,25 @@ $(document).ready(function () {
     $(".button-collapse").sideNav();
     $('select').material_select();
 
+    addHTMLToNav();
 });
+
+
+var loaded = false;
+
+var addHTMLToNav= function(){
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var quizId = url.searchParams.get("id");
+    var html = `<li><div class="divider"></div></li>
+              <li><a href="aantal.php?id=${quizId}"><i class="material-icons">format_list_numbered</i>Quiz Template</a></li>
+              <li><a href="moeilijkheidsgraad.php?id=${quizId}"><i class="material-icons">format_align_left</i>Moeilijkheidsgraad</a></li>
+              <li><a href="categories.php?id=${quizId}"><i class="material-icons">sort</i>Categorieën</a></li>
+              <li><a href="dashboard.php?id=${quizId}"><i class="material-icons">equalizer</i>Statistics</a></li>`;
+
+    if(quizId != null && url.pathname == '/editquestions.php' && !loaded) {
+        $("#slide-out").append(html);
+        loaded = true;
+    }
+
+}
