4d2e1e7d3575c8ad36e857b700b102b980ee17d2
Sky_Dragonsz <Arthur_Saprunov@hotmail.com>
2018-01-12 22:32:55 +0100

    Changed accordeon to be more userfriendly

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -2,9 +2,15 @@ $(document).ready(function () {
     $(".dropdown-button").dropdown();
     $(".button-collapse").sideNav();
     $('select').material_select();
+    $('.collapsible').collapsible({accordion:true,
+                                    onClose: function(e) {  $(e).find("i").text("keyboard_arrow_right");},
+                                    onOpen: function(e) {  $(e).find("i").text("keyboard_arrow_down");}
+                                });
 
     addHTMLToNav();
 });
+
+
 
 
 var loaded = false;
@@ -14,7 +20,7 @@ var addHTMLToNav= function(){
     var url = new URL(url_string);
     var quizId = url.searchParams.get("id");
     var html = `<li><div class="divider"></div></li>
-            <li><a href="editquestions.php?id=${quizId}"><i class="material-icons">format_list_numbered</i>Vragen</a></li>
+            <li><a href="editquestions.php?id=${quizId}"><i class="material-icons">label</i>Vragen</a></li>
             <li><a href="aantal.php?id=${quizId}"><i class="material-icons">format_list_numbered</i>Quiz Template</a></li>
             <li><a href="moeilijkheidsgraad.php?id=${quizId}"><i class="material-icons">format_align_left</i>Moeilijkheidsgraad</a></li>
             <li><a href="categories.php?id=${quizId}"><i class="material-icons">sort</i>Categorieën</a></li>
