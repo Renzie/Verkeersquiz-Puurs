8f504fe780951df5e66f641344dc21ee40c0db0c
Sky_Dragonsz <Arthur_Saprunov@hotmail.com>
2018-01-12 14:58:35 +0100

    Fixed little mistake in script.js

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -14,12 +14,13 @@ var addHTMLToNav= function(){
     var url = new URL(url_string);
     var quizId = url.searchParams.get("id");
     var html = `<li><div class="divider"></div></li>
-              <li><a href="aantal.php?id=${quizId}"><i class="material-icons">format_list_numbered</i>Quiz Template</a></li>
-              <li><a href="moeilijkheidsgraad.php?id=${quizId}"><i class="material-icons">format_align_left</i>Moeilijkheidsgraad</a></li>
-              <li><a href="categories.php?id=${quizId}"><i class="material-icons">sort</i>Categorieën</a></li>
-              <li><a href="dashboard.php?id=${quizId}"><i class="material-icons">equalizer</i>Statistics</a></li>`;
+            <li><a href="editquestions.php?id=${quizId}"><i class="material-icons">format_list_numbered</i>Vragen</a></li>
+            <li><a href="aantal.php?id=${quizId}"><i class="material-icons">format_list_numbered</i>Quiz Template</a></li>
+            <li><a href="moeilijkheidsgraad.php?id=${quizId}"><i class="material-icons">format_align_left</i>Moeilijkheidsgraad</a></li>
+            <li><a href="categories.php?id=${quizId}"><i class="material-icons">sort</i>Categorieën</a></li>
+            <li><a href="dashboard.php?id=${quizId}"><i class="material-icons">equalizer</i>Statistics</a></li>`;
 
-    if(quizId != null && url.pathname == '/editquestions.php' && !loaded) {
+    if(quizId != null && url.pathname != '/editorganization.php' && !loaded) {
         $("#slide-out").append(html);
         loaded = true;
     }
