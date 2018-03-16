d5e5b2a9514b8f4c1e3c5a5a02aa800a417465ee
Sky_Dragonsz <Arthur_Saprunov@hotmail.com>
2018-03-16 17:06:54 +0100

    Made the error page "more beautiful"!

--- website/error.php
+++ website/error.php
@@ -1,5 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Error</title>
+        <link href="assets/css/reset.css" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="assets/materialize/css/materialize.min.css" rel="stylesheet" media="screen,projection"/>
+        <link href="assets/css/screen.css" rel="stylesheet"/>
+        <style>
+            #errorMsg{
+                margin:20px;
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="loader-wrapper">
+            <div id="loader"></div>
+            <div class="loader-section section-left"></div>
+            <div class="loader-section section-right"></div>
+        </div>
+            <div class="section"></div>
+            <main class="center">
+
+                <h5 class="indigo-text">Quizplatform</h5>
+                <div class="section"></div>
+
+
+                    <div class="loginpanel z-depth-1 grey lighten-4 row"
+                         style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;">
+
+
+
+<p id="errorMsg">
+
+
 <?php
-require_once "head.html";
+
   $error_msg = array();
   if (isset($_GET['q']) && is_numeric($_GET['q'])) {
     $status = array(
@@ -18,11 +55,19 @@ require_once "head.html";
   }
   if (!empty($error_msg)) {
     foreach ($error_msg as $err) {
-      echo $err . '<br>';
+      echo $err . '<br><br>';
     }
   }
   else {
     echo 'Something went wrong.';
   }
-  require_once "tail.html";
+
 ?>
+</p>
+</div>
+
+<div class="section"></div>
+<div class="section"></div>
+</main>
+</body>
+</html>
